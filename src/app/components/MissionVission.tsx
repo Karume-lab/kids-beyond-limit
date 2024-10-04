d5d6ff1d1e6930a90a14f5cc64a974a052bec270
Karume-lab <danielkarume@outlook.com>
2024-10-04 08:08:20 +0300

    feat: :sparkles: finish mission/vission section

--- src/app/components/MissionVission.tsx
+++ src/app/components/MissionVission.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+
 
 const MissionVission = () => {
     return (
-        <div>
-            Mission Vission
+        <div className='bg-[url("/hero-bg.png")] bg-no-repeat bg-cover py-64 text-white flex flex-col gap-20 mt-20'>
+            <Section
+                title="MISSION"
+                paragraph={`
+                To use sports as a tool to unlock the potential of every child by 
+                creating pathways to education, leadership, and personal development, 
+                promoting a healthier and more inclusive society
+                `}
+            />
+            <Section
+                title="VISION"
+                paragraph={`
+                A world where every child can access education, live a healthy life,
+                and contribute to the betterment of society through sports and leadership 
+                development.
+                `}
+            />
         </div>
     )
 }
 
 export default MissionVission;
+
+
+type SectionProps = {
+    title: string,
+    paragraph: string,
+}
+
+
+const Section: React.FC<SectionProps> = ({
+    title,
+    paragraph,
+}) => {
+    return (
+        <div className="w-fit flex items-center justify-center flex-col mx-auto gap-5">
+            <h2 className="text-6xl underline">{title}</h2>
+            <p className="w-3/5 text-xl">
+                {paragraph}
+            </p>
+        </div>
+    );
+}
--- src/app/components/ReachingLives.tsx
+++ src/app/components/ReachingLives.tsx
@@ -17,7 +17,7 @@ const ReachingLives = () => {
                 </Button>
             </div>
             <div className="relative">
-                <div className="bg-white shadow-2xl rounded-xl w-4/5 px-32 py-5 z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="bg-white shadow-lg rounded-xl w-4/5 px-32 py-5 z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                     <h2 className="font-bold italic text-xl text-black">Reaching Lives</h2>
                     <ul className="text-orange-primary flex justify-between items-center">
                         {statistics.map(({ name, value }) => {
