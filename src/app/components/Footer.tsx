b39671ee2f32d8a12f0e52fbe94774ba4f33c5c9
Karume-lab <danielkarume@outlook.com>
2024-10-06 11:56:20 +0300

    feat: :sparkles: finish footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import KblLogo from './KblLogo';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='bg-[#184354] h-96'>
+            <div className='py-10 px-20'>
+                <KblLogo />
+            </div>
         </div>
     )
 }
