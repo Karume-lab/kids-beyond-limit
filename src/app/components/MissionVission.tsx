667f807a8a44afd04a827e35c82185e068a0fab3
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-10-10 15:16:26 +0300

    style: :lipstick: Added parralax scrolling effect

--- src/app/components/MissionVission.tsx
+++ src/app/components/MissionVission.tsx
@@ -2,7 +2,7 @@
 
 const MissionVission = () => {
     return (
-        <div className='bg-[url("/hero-bg.png")] bg-no-repeat bg-cover py-64 text-white flex flex-col gap-20 mt-20'>
+        <div className='bg-[url("/hero-bg.png")] bg-no-repeat bg-cover bg-fixed py-32 md:py-64 text-white flex flex-col gap-20 mt-10 md:mt-20'>
             <Section
                 title="MISSION"
                 paragraph={`
@@ -38,8 +38,8 @@ const Section: React.FC<SectionProps> = ({
 }) => {
     return (
         <div className="w-fit flex items-center justify-center flex-col mx-auto gap-5">
-            <h2 className="text-6xl underline">{title}</h2>
-            <p className="w-3/5 text-xl">
+            <h2 className="text-3xl md:text-6xl underline">{title}</h2>
+            <p className="px-10 md:w-3/5 md:text-2xl text-md text-center">
                 {paragraph}
             </p>
         </div>
