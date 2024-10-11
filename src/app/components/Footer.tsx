359afbae01a487fbc4ff6dfa04527ecb79bd6b51
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-10-11 08:10:05 +0300

    style: :lipstick: Footer image

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import KblLogo from './KblLogo';
+import Image from 'next/image';
 
 const Footer = () => {
     return (
         <div className='bg-[#184354] h-96'>
             <div className='py-10 px-20'>
-                <KblLogo />
+                <Image
+                    src="/logo-no-backg.png"
+                    alt='KBL Logo'
+                    width={100}
+                    height={100}
+                    className=''
+                />
             </div>
         </div>
     )
--- src/app/components/MoreThanASport.tsx
+++ src/app/components/MoreThanASport.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+ import React from 'react'
 import RoundedImageCard from './RoundedImageCard';
 import { benefits } from '@/lib/constants';
+// import Image from 'next/image';
+import { Trophy } from 'lucide-react';
 
 const MoreThanASport = () => {
     return (
-        <div className='bg-orange-gradient text-white flex py-20 px-64 justify-between'>
+        <div className='bg-orange-gradient text-white flex flex-wrap-reverse py-20 px-8 md:px-64 justify-between'>
             <RoundedImageCard
                 imageUrl='/more-than-a-sport-bg-img.png'
                 alt='Girl throwing ball'
                 size={500}
             />
-            <div className='w-1/2'>
-                <h2 className='text-3xl font-bold'>More than a sport: Handball for hope.</h2>
+            <div className=' md:w-1/2'>
+                <h2 className=' text-2xl md:text-3xl font-bold'>More than a sport: Handball for hope.</h2>
                 <p className='my-5'>
                     We believe that sports is a school  that allows learning
                     in a playful and fun manner, and gain fundamental skills.
@@ -19,14 +21,18 @@ const MoreThanASport = () => {
                 <ul className='flex flex-col gap-3'>
                     {benefits.map(benefit => {
                         return (
-                            <li key={benefit} className='flex gap-2'>
-                                <RoundedImageCard
-                                    imageUrl='/footbal-icon.png'
-                                    alt='Football icon'
-                                    size={500}
-                                />
-                                <span>{benefit}</span>
-                            </li>
+                            <div key={benefit} className='flex items-center'>
+                                <li className='h-1/2'>
+                                    {/* <Image
+                                        src='/footbal-icon.png'
+                                        alt='Football icon'
+                                        width={20}
+                                        height={20}
+                                    /> */}
+                                    <Trophy />
+                                </li>
+                                <span className='h-2/3'>{benefit}</span>
+                            </div>
                         );
                     })}
                 </ul>
