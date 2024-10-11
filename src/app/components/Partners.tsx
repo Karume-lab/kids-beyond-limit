e87efe127d4fdc72e4680b5333240b30372d73c2
Karume-lab <danielkarume@outlook.com>
2024-10-11 10:28:03 +0300

    fix: :iphone: add spacing

--- src/app/components/Partners.tsx
+++ src/app/components/Partners.tsx
@@ -7,8 +7,8 @@ import Marquee from 'react-fast-marquee';
 
 const Partners = () => {
     return (
-        <div className="p-8 bg-[#F9F9F9]">
-            <h2 className='text-blue-600 text-center text-3xl font-bold my-5'>Our Valuable Patners</h2>
+        <div className="p-8 bg-[#F9F9F9] mb-20">
+            <h2 className='text-blue-600 text-center text-3xl font-bold my-5 py-10'>Our Valuable Patners</h2>
             <div className='hidden md:block'>
                 <Marquee autoFill pauseOnHover speed={70} >
                         {
